3f37a6a1beed9d31ff4bcb3540d21a86b97bfa7f
Alexander <al.x.falconer@gmail.com>
2021-04-05 17:14:07 -0400

    commit tree 22916fdcd40f5c6b07237dc1ec6ed66a8d6778cf

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let multiply = 62;
+let num1 = 31;
+let num2 = 2;
+num1 * num2;
 
-// Write your code here
+let random = Math.ceil(Math.random() * 10) + 1;
+
+let mod = 4;
+let num3 = 16;
+let num4 = 4;
+num3 % num4;
+
+let max = 20;
+Math.max(1, 5, 20);
